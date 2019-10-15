2ff204975134c66873fca031fde0930815533811
trinhhuy <trinhhuyk57@gmail.com>
2019-10-15 22:10:57 +0700

    update get price

--- src/routes/api/ghnController.js
+++ src/routes/api/ghnController.js
@@ -4,47 +4,33 @@ const axios = require('axios');
 function ghnController() {
   const self = {
     INFO_DELIVERY: Setting.IS_PRODUCTION ? Setting.PRODUCTION.GHN : Setting.LOCAL.GHN,
-    getPriceFromDelivery: (req, res) => {
-      let dataRequest = req.dataRequest;
-
-      return axios.post(self.INFO_DELIVERY.domain + self.INFO_DELIVERY.price_url, dataRequest).then(response => {
-        return res.json({s: 200, data: response})
-      }).catch(error => {
-        let data = error.response
-        return res.json({s: 200, data: data.data})
-      })
-    }
-    
   };
 
   return {
     login: (req, res) => {
 
     },
-    getPrice: (req, res) => {
-        let dataRequest = req.body;
-        let services = dataRequest.services
+    getPrice: async (req, res) => {
+        let services = req.body.services
+        req.body.token = self.INFO_DELIVERY.token;
 
-        dataRequest.token = self.INFO_DELIVERY.token;
-
-        let functions = []
-        for (let i = 0; i < services.length; i++) {
-          dataRequest.ServiceID = services[i]
-          req.dataRequest = dataRequest
-
-        }
-      return Promise.all([
-        self.getPriceFromDelivery()
-      ]).then(res => {
-
-      })
-
-        // return axios.post(self.INFO_DELIVERY.domain + self.INFO_DELIVERY.price_url, dataRequest).then(response => {
-        //   return res.json({s: 200, data: response})
-        // }).catch(error => {
-        //   let data = error.response
-        //   return res.json({s: 200, data: data.data})
-        // })
+        let result = []
+        await Promise.all(
+          services.map(service => {
+            let dataRequest = JSON.parse(JSON.stringify(req.body))
+            dataRequest.ServiceID = service
+            return axios.post(self.INFO_DELIVERY.domain + self.INFO_DELIVERY.price_url, dataRequest).then(response => {
+              let data = response.data
+              data.serviceId = service
+              result.push(response.data)
+            }).catch(error => {
+              let data = error.response.data
+              data.serviceId = service
+              result.push(data)
+            })
+          })
+        )
+        return res.json({s: 200, data: result})
     }
   }
 }
