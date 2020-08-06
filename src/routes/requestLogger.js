bf00209f970883f0c6a6b92647dcfe9dcba385dc
ThieuLM <thieu.lm@citigo.com.vn>
2020-08-06 14:49:05 +0700

    add log for nodejs

---
+++ src/routes/requestLogger.js
@@ -0,0 +1,22 @@
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'shipping-price-request-%DATE%.log',
+  dirname: './logging',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '50m',
+  maxFiles: '7d'
+});
+
+transport.on('rotate', function(oldFilename, newFilename) {
+  // do something
+});
+
+const requestLogger = winston.createLogger({
+  transports: [
+    transport
+  ]
+});
+module.exports = requestLogger
